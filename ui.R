1f96d9e996a8de98f6db041c77a76a8d806da677
Aulide81 <emiliojat@gmail.com>
2016-10-24 11:01:43 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -31,7 +31,7 @@ shinyUI(
                               ),
                               mainPanel(
                                 mainPanel(
-                                plotOutput('plot1', width ="2000",height = "600px"),
+                                plotOutput('plot1', width ="1800",height = "800px"),
                                 textOutput("ruta")
                               )
                             )
