25eebb3b47d3279e398b803d14ae7ed084e7061b
Aulide81 <emiliojat@gmail.com>
2016-10-24 13:02:07 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -26,8 +26,8 @@ shinyUI(
                                 sliderInput("dim","Dim Draw",min=1,max=10,value=c(1,2)),
                                 br(),
                                 numericInput("ndim", "Numero Dimensiones", 2, min = 1,width =100),
-                                downloadButton("descarga","Descargar Dimensiones"),
-                                downloadButton("descarga2","Descargar Gráfico"),
+                                downloadButton("descarga","Dimensiones"),
+                                downloadButton("descarga2","Gráfico"),
                                 width=2
                               ),
                               mainPanel(
